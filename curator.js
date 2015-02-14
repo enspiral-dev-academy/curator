26f7da8086f6d5d93f615c6972880c769e21e52c
amelialaundy <amelialaundy@gmail.com>
2015-02-14 20:16:34 +1300

    curator.js fully tested

--- curator.js
+++ curator.js
@@ -5,27 +5,29 @@ var controllers = require('./lib/controllers/index');
 var Errors = require('./lib/errors/index');
 var helpers = require('./lib/helpers/index');
 
-var argument = input.shift();
+var start = function () {
+	var argument = input.shift();
+	switch (argument) {
+		case 'init':
+			var init = new controllers.init();
+			init.initializeFolders(input);
+			break;
+		case 'languages':
+			var string = "Currently available languages:";
+			console.log(helpers.displayLanguages(string));
+			break;
+		case "--help":
+			Errors.argument.help();
+			break;
+		default:
+			Errors.argument.help();
+			break;
+	}
+};
 
-switch (argument) {
-	case 'init':
-		var init = new controllers.init();
-		init.initializeFolders(input);
-		break;
-	case 'languages':
-		var string = "Currently available languages:";
-		console.log(helpers.displayLanguages(string));
-		break;
-	case undefined:
-		Errors.argument.help();
-		break;
-	case "--help":
-		Errors.argument.help();
-		break;
-	default:
-		Errors.argument.invalidCommand(argument);
-		break;
-}
+module.exports = start();
+// start();
+// module.exports = 'start()';
 // pass the command line args
 //  probably a router class of some kind
 // each command has it's own controller - build, init
